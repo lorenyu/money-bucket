176c42170f88c6b8740724bd6c9e8f2de2b69a75
Loren Yu <yuloren@gmail.com>
2012-07-23 00:07:51 -0700

    added BucketService.getBucketsForUser

--- services/BucketService.js
+++ services/BucketService.js
@@ -1,11 +1,22 @@
 var Storage = require('./Storage'),
-    Bucket = require('../models/Bucket');
+    Bucket = require('../models/Bucket'),
+    db = require('../db');
 
 var BucketService = module.exports = {
-  loadBucket: function(id, callback) {
+  getBucket: function(id, callback) {
     Storage.load(Bucket, id, callback);
   },
   saveBucket: function(bucket, callback) {
     Storage.save(Bucket, bucket, callback);
+  },
+  getBucketsForUser: function(user, callback) {
+    db.collection(Bucket.collectionName, function(err, collection) {
+      collection.find({ 'userId': user._id }).toArray(function(err, buckets) {
+        buckets = _.map(buckets, function(bucket) {
+          return new Bucket(bucket);
+        });
+        callback(null, buckets);
+      });
+    });
   }
 };
