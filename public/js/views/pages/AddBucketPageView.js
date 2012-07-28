b1e70e9af6ea5b0bc1413c3472be83afe6ef0ad6
Loren Yu <yuloren@gmail.com>
2012-07-28 11:09:54 -0700

    can create buckets

--- public/js/views/pages/AddBucketPageView.js
+++ public/js/views/pages/AddBucketPageView.js
@@ -2,11 +2,24 @@ MB.namespace('views.pages');
 
 MB.views.pages.AddBucketPageView = Backbone.View.extend({
   events: {
+    'submit form': 'addBucket'
   },
   initialize: function(options) {
   },
   render: function() {
     this.$el.html(MB.render.buckets.add({}));
     return this;
+  },
+  addBucket: function(event) {
+    event.preventDefault();
+    var bucket = this.model.get('buckets').create({
+      name: this.$el.find('[name=name]').val(),
+      description: this.$el.find('[name=description]').val()
+    });
+    if (!bucket) {
+      console.log('Cannot create bucket');
+      return;
+    }
+    MB.router.go('/');
   }
 });
