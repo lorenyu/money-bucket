e46112a58b0f428c590a923ca634003974db144c
Loren Yu <yuloren@gmail.com>
2012-07-28 11:19:09 -0700

    fixed: anchor links and removed the need to force the navigate in code

--- public/js/views/pages/HomePageView.js
+++ public/js/views/pages/HomePageView.js
@@ -2,7 +2,6 @@ MB.namespace('views.pages');
 
 MB.views.pages.HomePageView = Backbone.View.extend({
   events: {
-    'click .add-bucket':      'addBucket'
   },
   initialize: function(options) {
     var self = this;
@@ -14,8 +13,5 @@ MB.views.pages.HomePageView = Backbone.View.extend({
   render: function() {
     this.$el.html(MB.render.home({ buckets: this.model.get('buckets').toJSON() }));
     return this;
-  },
-  addBucket: function() {
-    MB.router.go('buckets/add');
   }
 });
