d826f38a4aee722b854a199d4e9ff0769557a2c7
Loren Yu <yuloren@gmail.com>
2012-07-29 11:16:24 -0700

    Refixed the bug - i must have removed the onreset/onsync handlers for the buckets at some point

--- public/js/views/pages/BucketsPageView.js
+++ public/js/views/pages/BucketsPageView.js
@@ -4,6 +4,8 @@ MB.views.pages.BucketsPageView = Backbone.View.extend({
   events: {
   },
   initialize: function(options) {
+    this.model.get('buckets').on('reset', _.bind(this.render, this));
+    this.model.get('buckets').on('sync', _.bind(this.render, this));
   },
   render: function() {
     this.$el.html(MB.render.buckets.buckets({ buckets: this.model.get('buckets').toJSON() }));
