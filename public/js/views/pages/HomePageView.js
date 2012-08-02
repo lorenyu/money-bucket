eefaa7239432d87a11d670b6c2a61ec4984a6e65
Loren Yu <yuloren@gmail.com>
2012-08-02 01:54:06 -0700

    removed hero units on home page and replaced it with simple summary of buckets

--- public/js/views/pages/HomePageView.js
+++ public/js/views/pages/HomePageView.js
@@ -11,7 +11,9 @@ MB.views.pages.HomePageView = Backbone.View.extend({
   render: function() {
     this.$el.html(MB.render.pages.home({
       user: this.model.toJSON(),
-      buckets: this.model.get('buckets').toJSON()
+      buckets: this.model.get('buckets').toJSON(),
+      allocatedAmount: this.model.allocatedAmount(),
+      unallocatedAmount: this.model.unallocatedAmount()
     }));
     return this;
   },
