6fcb98039dcaa285f9659789140ef8aefbcaf99b
Loren Yu <yuloren@gmail.com>
2012-08-25 00:59:23 -0700

    removed unused BucketCollectionView

--- public/js/views/BucketCollectionView.js
+++
@@ -1,9 +0,0 @@
-MB.namespace('views');
-
-MB.views.BucketCollectionView = Backbone.View.extend({
-  className: 'buckets',
-  render: function() {
-    this.$el.html(MB.render.buckets.buckets({ buckets: this.collection }));
-    return this;
-  }
-});
