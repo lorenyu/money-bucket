212e8d294692a4154ded5e5b1c050d3509619047
Loren Yu <yuloren@gmail.com>
2012-08-25 01:01:11 -0700

    removed unused BucketView

--- public/js/views/BucketView.js
+++
@@ -1,9 +0,0 @@
-MB.namespace('views');
-
-MB.views.BucketView = Backbone.View.extend({
-  className: 'bucket',
-  render: function() {
-    this.$el.html(MB.render.buckets.bucket({ bucket: this.model }));
-    return this;
-  }
-});
