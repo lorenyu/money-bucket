4318768898525dab2d5151018a41562aab75cd94
Loren Yu <yuloren@gmail.com>
2012-10-05 23:46:06 -0700

    User can log in from any page after trying out the site. If the user already exists then their data is updated. If it is a new user the page should save all the buckets for that user.

--- public/js/views/pages/LoginPageView.js
+++ public/js/views/pages/LoginPageView.js
@@ -2,24 +2,11 @@ MB.namespace('views.pages');
 
 MB.views.pages.LoginPageView = Backbone.View.extend({
   events: {
-    // TODO: the header .login should be in a more general place not just LoginPageView
-    'click .login': 'login'
   },
   initialize: function(options) {
   },
   render: function() {
     this.$el.html(MB.render.pages.login({}));
     return this;
-  },
-  login: function() {
-    MB.auth.promptUserForLogin(function(data) {
-      MB.user = new MB.models.User(data.user);
-      MB.user.get('buckets').fetch({
-        success: function() {
-          // backbone's equivalent to refresh
-          Backbone.history.loadUrl(window.location.hash);
-        }
-      });
-    });
   }
 });
